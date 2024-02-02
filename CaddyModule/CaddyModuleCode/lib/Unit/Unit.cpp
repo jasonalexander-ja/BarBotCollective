cc8ae31c0aee540be785386215e13c6e94942a67
JSON Alexander <61407072+jasonalexander-ja@users.noreply.github.com>
2024-02-02 23:21:17 +0000

    Woked on controller code

--- CaddyModule/CaddyModuleCode/lib/Unit/Unit.cpp
+++ CaddyModule/CaddyModuleCode/lib/Unit/Unit.cpp
@@ -2,103 +2,117 @@
 #include <Wire.h>
 #include <Unit.h>
 #include <Stepper.h>
+#include <AccelStepper.h>
 #include "Adafruit_VL53L0X.h"
 
-/*
-This is where all your code should live for your module. 
-
-Place all code you want to execute on startup in `void unitSetup()`, 
-this is like `setup()` when programming regular Arduino. 
-
-The code you want to run when the control unit requests an action should 
-live inside `int performAction(int option)`. 
-
-All Arduino functions and concepts will work here.  
-
-If you have a simple unit that say moves a servo back and forth to 
-dispense a shot, you can just add the code to do this and add the 
-`return 0` at the end. 
-
-Here's an example that just flashes an LED: 
-
-int performAction(int option)
-{
-    digitalWrite(led1, HIGH);
-    delay(500);
-    digitalWrite(led1, LOW);
-    delay(500);
-    return 0;
-}
-
-If your module can do multiple different things, such as light up different 
-LEDs, we can assign each of these a number between 0 and 255 in the 
-control unit, you will then receive the number in the `option` parameter 
-that is passed into the `performAction` function. 
-
-Example: 
-
-int performAction(int option)
-{
-    switch(option)
-    {
-        case 1: 
-            digitalWrite(led1, HIGH);
-            break;
-        case 2: 
-            digitalWrite(led2, HIGH);
-            break;
-        default: 
-            digitalWrite(led1, HIGH);
-            digitalWrite(led2, HIGH);
-    }
-    return 0;
-}
-
-If you want to communicate back an error to the control unit, you can simply 
-return a number between 1 and 255 from the `performAction` function (0 means
-no error/sucessful). 
-
-Here's an example: 
-
-int performAction(int option)
-{
-    int temperature = analogRead(ADC_TEMP);
-
-    if (temperature > 900)
-    {
-        return 3; // Too hot! 
-    }
-
-    return 0;
-}
-
-Once a return is hit in `performAction` function, no more code will be executed
-until another request is made, it's just jumping to the end of the function. 
-
-Happy Hacking! 
-*/
-
 #define STEPS 800
+#define MAX_RANGE 1000 
+#define MM_PER_POSITION 100
+#define VARIATION 10
 
-Stepper stepper(STEPS, 12, 13);
+int zeroOffset = 0;
+
+AccelStepper stepper(AccelStepper::MotorInterfaceType::DRIVER, 12, 13);
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 bool loxOk = false;
 
+void waitForCupPlaced();
+void waitForCupRemoved();
+void setZeroOffset();
+int moveToPosition(int pos);
+int getDistance();
+
 void unitSetup()
 {
-    // Put your code in here to run once on setup 
+    stepper.setMaxSpeed(100);
+    stepper.setAcceleration(20);
+    Wire1.setPins(2, 15);
     loxOk = lox.begin(VL53L0X_I2C_ADDR, false, &Wire1);
-    Wire1.setPins(33, 32);
 }
 
 int performAction(int option)
-{
-    // Put your code here to run when an action is requested 
+{ 
     if (!loxOk)
         return CaddyErrors::DistSensorInitError;
-        
+    
+    switch (option)
+    {
+        case 255:
+            waitForCupPlaced();
+            break;
+        case 254:
+            waitForCupRemoved();
+            break;
+        case 253: 
+            setZeroOffset();
+            break;
+        default:
+            return moveToPosition(option);
+    }
+
     // Return 0 when ran sucessfully 
     return 0;
+}
+
+int moveToPosition(int pos)
+{
+    int target = (pos * MM_PER_POSITION) - (MM_PER_POSITION / 2); 
+    return 0;
+}
+
+int getDistance()
+{
+    VL53L0X_RangingMeasurementData_t measure;
+    for (int i = 0; i < 10; i++)
+    {
+        lox.rangingTest(&measure, false);
+        if (measure.RangeStatus != 4 && 
+            measure.RangeMilliMeter - zeroOffset <= MAX_RANGE)
+        {
+            return measure.RangeMilliMeter - zeroOffset;
+        }
+    }
+    return -1;
+}
+
+void setZeroOffset()
+{
+    VL53L0X_RangingMeasurementData_t measure;
+    lox.rangingTest(&measure, false);
+    if (measure.RangeStatus != 4 && measure.RangeMilliMeter <= MAX_RANGE)
+    {
+        zeroOffset = measure.RangeMilliMeter;
+    }
+}
+
+void waitForCupPlaced()
+{
+    VL53L0X_RangingMeasurementData_t measure;
+    while (true)
+    {
+        lox.rangingTest(&measure, false);
+        if (measure.RangeMilliMeter == 4 || measure.RangeMilliMeter > MAX_RANGE)
+        {
+            delay(2000);
+            return;
+        }
+        delay(200);
+    }
+}
+
+void waitForCupRemoved()
+{
+    VL53L0X_RangingMeasurementData_t measure;
+    while (true)
+    {
+        lox.rangingTest(&measure, false);
+        if (measure.RangeMilliMeter != 4 && measure.RangeMilliMeter <= MAX_RANGE)
+        {
+            delay(2000);
+            return;
+        }
+        delay(200);
+    }
 }
 
